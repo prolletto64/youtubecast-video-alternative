59129fb7b7160a1773b30bcbd86644e2046c081b
prolletto64 <poiluonzo@mail.com>
2024-09-18 11:40:47 +0200

    request implementation

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,11 @@ COPY ./src ./
 RUN npm run build
 
 FROM node:22-alpine
+RUN apk add --no-cache yarn ffmpeg python3 py3-pip bash rsync
 WORKDIR /app
+RUN set -x && \
+  wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp && \
+  chmod +x ./yt-dlp
 COPY ./package.json ./
 RUN npm install
 COPY --from=build /usr/src/app/dist/index.js ./index.js
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,33 @@
 import express, { Express, Request, Response } from "express";
+import { execSync } from "node:child_process";
+import fs from "fs";
 
 const app: Express = express();
 const port = 80;
 
-app.get("*", (req: Request, res: Response) => {
+app.get("/:video", (req: Request, res: Response) => {
+  if (!req.params.video.match(/^[a-z0-9_-]{11}$/i)) {
+    res.statusCode = 400;
+    res.send("not a video id...");
+    return;
+  }
+  try {
+    console.log(`Getting video streaming link for ${req.params.video}`);
+    const url = execSync(
+      `yt-dlp -g --format=best[vcodec^=avc1] ${
+        fs.existsSync("/app/cookies.txt") ? `--cookies="/app/cookies.txt"` : ""
+      } http://www.youtube.com/watch?v=${req.params.video}`
+    ).toString();
+    res.send(url);
+    return;
+  } catch (error: any) {
+    console.error(error);
+    res.sendStatus(500);
+    return;
+  }
+});
+
+app.get("/", (req: Request, res: Response) => {
   console.log(
     JSON.stringify(req, [
       "bodyUsed",
@@ -46,6 +70,7 @@ app.post("*", (req: Request, res: Response) => {
       "url",
     ])
   );
+  console.log(JSON.stringify(req.body));
   res.send("");
 });
 
